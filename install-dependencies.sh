c842287ad187fefaa8de99fedb8c78b007e54646
Filip Rysavy <fil@nebulous.tech>
2020-06-22 13:45:50 +0200

    Revert removing codespell install, useful for local build

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,6 +6,14 @@ if ! [ -x "$(command -v golangci-lint)" ]; then
 	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.24.0
 fi
 
+if ! [ -x "$(command -v codespell)" ]; then
+  echo "Installing codespell..."
+  if ! [ -x "$(command -v pip3)" ]; then
+    sudo apt install python3-pip
+  fi
+  pip3 install codespell
+fi
+
 if ! [ -x "$(command -v analyze)" ]; then
   echo "Installing analyze..."
   go get gitlab.com/NebulousLabs/analyze
