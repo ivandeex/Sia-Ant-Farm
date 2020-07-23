a1c2105825dc0257e813faf0adf9608de941780c
Filip Rysavy <fil@nebulous.tech>
2020-07-23 13:48:41 +0200

    Get Sia versions to build automatically

--- scripts/build-sia-dev-binaries.sh
+++ scripts/build-sia-dev-binaries.sh
@@ -1,31 +1,40 @@
 #!/usr/bin/env bash
 set -e
 
-# This script generates siad-dev binaries of the given versions and stores them
+# This script generates siad-dev binaries of the versions greater than or equal
+# to the given version set in get-versions.sh script (v1.3.7) and stores them
 # in target folder.
-# Requires `Sia` repo to be sibling directory of this (`Sia-Ant-Farm`) repo
-# otherwise the 2nd pushd path must be updated.
+#
+# Requires:
+# - Sia repo to be sibling directory of this (`Sia-Ant-Farm`) repo
+#   otherwise the 2nd pushd path must be updated.
+# - get-versions.sh script with its own requirements
 
 # set working dir to script location
 pushd $(dirname "$0") > /dev/null
 
 # set target folder
-target_folder=$(realpath ../upgrade-binaries)
-
-# set working dir to Sia repo
-pushd ../../Sia > /dev/null
+export target_folder=$(realpath ../upgrade-binaries)
 
 # setup build-time vars
 ldflags="-s -w -X 'gitlab.com/NebulousLabs/Sia/build.GitRevision=`git rev-parse --short HEAD`' -X 'gitlab.com/NebulousLabs/Sia/build.BuildTime=`git show -s --format=%ci HEAD`' -X 'gitlab.com/NebulousLabs/Sia/build.ReleaseTag=${rc}'"
 
-function build {
+# build_version checks out the given version of Sia Gitlab repo, builds
+# siad-dev binary and stores the binary in the subfolder of the $target_folder.
+# Inputs:
+# - $1: Sia version (git tag) to build
+function build_version {
   version=$1
   os=linux
   arch=amd64
 
   echo Building $version...
+
+  # set working dir to Sia repo
+  pushd ../../Sia > /dev/null
   
   # checkout the version
+  git reset --hard HEAD
   git checkout $version
   
   # create workspace
@@ -55,13 +64,15 @@ function build {
     git checkout master
     popd > /dev/null
   fi
+
+  popd > /dev/null
 }
+export -f build_version
 
-# Build dev binaries.
-for version in "v1.3.7" "v1.4.0" "v1.4.1" "v1.4.1.1" "v1.4.1.2" "v1.4.2.0" "v1.4.3" "v1.4.4" "v1.4.5" "v1.4.6" "v1.4.7" "v1.4.8" "v1.4.10" "v1.4.11" "master"
-do
-  build $version
-done
+# Build dev binaries for Sia releases and latest master.
+# Use get-versions.sh to get all releases greater then or equal to the given
+# release (v1.3.7).
+./get-versions.sh | xargs -n 1 -I {} bash -c 'build_version "$@"' _ {}
+build_version master
 
-popd > /dev/null
 popd > /dev/null
