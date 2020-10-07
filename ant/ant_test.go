2b9b70ffed61a545fd6058457e21f5d9bded7e29
Filip Rysavy <fil@nebulous.tech>
2020-10-07 11:51:40 +0200

    Add failing test for checking closing ants

--- ant/ant_test.go
+++ ant/ant_test.go
@@ -1,8 +1,11 @@
 package ant
 
 import (
+	"os/exec"
+	"strings"
 	"sync"
 	"testing"
+	"time"
 
 	"gitlab.com/NebulousLabs/Sia-Ant-Farm/test"
 	"gitlab.com/NebulousLabs/Sia/node/api/client"
@@ -12,6 +15,72 @@ import (
 // newTestingAntConfig creates an AntConfig for testing.
 func newTestingAntConfig(datadir string) AntConfig {
 	return AntConfig{SiadConfig: newTestingSiadConfig(datadir)}
+}
+
+// TestClosingAnt tests closing the created Ant
+func TestClosingAnt(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Parallel()
+
+	// Create testing config
+	datadir := test.TestDir(t.Name())
+	config := newTestingAntConfig(datadir)
+
+	// Create Ant
+	ant, err := New(&sync.WaitGroup{}, config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create Sia Client
+	opts, err := client.DefaultOptions()
+	if err != nil {
+		t.Fatal(err)
+	}
+	opts.Address = config.APIAddr
+	c := client.New(opts)
+
+	// Test Sia is running by calling ConsensusGet endpoint
+	if _, err = c.ConsensusGet(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Close the ant
+	ant.Close()
+
+	// Wait enough time for ant.Close(), stopSiad() to try to finish
+	timeout := stopSiadTimeout + 10*time.Second
+	siadPath := config.SiadPath
+	APIAddress := config.APIAddr
+
+	start := time.Now()
+checkingLoop:
+	for {
+		// Timeout reached
+		if time.Since(start) > timeout {
+			t.Fatal("Siad process is still running")
+		}
+
+		// Get processes
+		out, err := exec.Command("ps", "-eo", "cmd").Output()
+		if err != nil {
+			t.Fatal("can't execute ps command")
+		}
+
+		// Parse processes
+		for _, p := range strings.Split(string(out), "\n") {
+			if strings.HasPrefix(p, siadPath) && strings.Contains(p, " --api-addr="+APIAddress+" ") {
+				// Siad process is still running
+				time.Sleep(time.Second)
+				continue checkingLoop
+			}
+		}
+
+		// Siad process is not running
+		return
+	}
 }
 
 // TestNewAnt tests creating an Ant
